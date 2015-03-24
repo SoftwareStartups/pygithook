3c3f0d350892feee267bfdc392f7c02ef81a0543
bram <bram@vectorfabrics.com>
2015-03-24 09:28:09 +0100

    VFG-0: Add install script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -x
+
+# Get the directory of this install script
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -6,5 +10,18 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-INSTALLDIR=$DIR/../../../vfgithook/install
-PYTHONPATH=$INSTALLDIR/lib:$PYTHONPATH $INSTALLDIR/bin/vf-pre-commit
+
+GITDIR=$(git rev-parse --show-toplevel)
+
+# Build the hooks
+pushd $DIR
+
+make test
+
+make
+
+popd
+
+# Add the symlinks
+ln -s $DIR/install/bin/pre-commit $GITDIR/.git/hooks/pre-commit
+
