101405ed90917c6061f051d208589233895292e4
bram <bram@vectorfabrics.com>
2015-03-25 12:19:25 +0100

    VFG-0: Fix rename of python_check module in test

--- tests/test_python_check.py
+++ tests/test_python_check.py
@@ -1,20 +1,21 @@
-"""Testsuite for vfgithook.python_check"""
+"""Testsuite for vfgithook.pylint_check"""
+
+from vfgithook import pylint_check
 
 from . import util
-from vfgithook import python_check
 
 
 def test_is_python_file(gitrepo):
-    """Test python_check.is_python_file"""
+    """Test pylint_check.is_python_file"""
 
     # Extension
     file_a = util.write_file(gitrepo, 'a.py', '')
-    assert python_check._is_python_file(file_a)
+    assert pylint_check._is_python_file(file_a)
 
     # Empty
     file_b = util.write_file(gitrepo, 'b', '')
-    assert not python_check._is_python_file(file_b)
+    assert not pylint_check._is_python_file(file_b)
 
     # Shebang
     file_c = util.write_file(gitrepo, 'b', '#!/usr/bin/env python')
-    assert python_check._is_python_file(file_c)
+    assert pylint_check._is_python_file(file_c)
